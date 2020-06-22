18db1976d827e083f2f4789a7ca8f9d08c35febd
Gertjan Bisschop <gertjanbisschop@gmail.com>
2020-06-22 22:20:43 +0100

    fixed error fix_pos_array

--- lib/simulate.py
+++ lib/simulate.py
@@ -143,7 +143,7 @@ def run_ind_sim(
         demographic_events=demographic_events,
         migration_matrix=migration_matrix,
         mutation_rate=theta,
-        random_seed=seed,
+        random_seed=seed, #error was when 3582573439
     )
 
     """
@@ -162,8 +162,10 @@ def run_ind_sim(
     # with infinite sites = pre-msprime 1.0
     positions = np.array([int(site.position) for site in ts.sites()])
     #print(f"[+] {ts.num_sites} mutation(s) along the simulated sequence")
-    new_positions = fix_pos_array(positions, total_length)
-    #print(seed)
+    new_positions = lib.gimble.fix_pos_array(positions)
+    if new_positions[-1] >= total_length:
+        blocklength = new_positions[-1]
+        total_length = blocks*blocklength
     genotype_matrix = get_genotypes(ts, ploidy, num_samples)
     sa_genotype_array = allel.GenotypeArray(genotype_matrix)
     # always the same for all pairwise comparisons
@@ -175,14 +177,12 @@ def run_ind_sim(
     for idx, pair in enumerate(comparisons):
         # slice genotype array
         subset_genotype_array = sa_genotype_array.subset(sel1=pair)
-        block_sites = np.arange(total_length).reshape(blocks, blocklength)
         block_sites_variant_bool = np.isin(
             block_sites, new_positions, assume_unique=True
         )
-        block_sites = lib.gimble.genotype_to_mutype_array(
+        result[idx] = lib.gimble.genotype_to_mutype_array(
             subset_genotype_array, block_sites_variant_bool, block_sites, debug=False
         )
-        result[idx] = block_sites
 
     return result
 
@@ -190,33 +190,6 @@ def run_ind_sim(
 def get_genotypes(ts, ploidy, num_samples):
     shape = (ts.num_mutations, num_samples, ploidy)
     return np.reshape(ts.genotype_matrix(), shape)
-
-
-def fix_pos_array(pos_array, total_length):
-    if len(pos_array) > total_length:
-        print(
-            "[-] more mutations than positions on simulated sequence, check mutation rate"
-        )
-        return np.unique(pos_array)
-    # print('pos_array_0', pos_array)
-    # get boolean array for first and subsequent duplicates (True) (required sorted)
-    idxs = np.insert((np.diff(pos_array) == 0).astype(np.bool), 0, False)
-    if np.any(idxs):
-        # if there are duplicates, get new values by incrementing by one
-        new_values = pos_array[idxs] + 1
-        # get non-duplicate values
-        uniq_values = pos_array[~idxs]
-        # insert new_values in non-duplicated values (required sorted)
-        new_idxs = np.searchsorted(uniq_values, new_values)
-        # recursive call
-        return fix_pos_array(
-            np.sort(np.insert(uniq_values, new_idxs, new_values)), total_length
-        )
-    else:
-        # if there are no duplicated values
-        # print('pos_array_1', pos_array)
-        return pos_array
-
 
 def dict_product(d):
     return [dict(zip(d, x)) for x in itertools.product(*d.values())]
