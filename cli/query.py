581e7ec00920dbd7ddb1093474624e683f061ce0
Dominik R Laetsch <dominik.laetsch@gmail.com>
2020-08-14 11:47:04 +0100

    comments

--- cli/query.py
+++ cli/query.py
@@ -6,12 +6,27 @@
     Options:
         -h --help                                   show this
         -z, --zarr_file FILE                        ZARR datastore
-        -b, --blocks                                Writes BED file of Blocks
+        -b, --blocks                                Writes BED file of blocks
+        -B, --blocks_bsfs                           Writes bSFS of blocks
+        -w, --windows                               Writes BED file of windows
+        -W, --windows_bsfs                          Writes bSFS of windows
         -u, --max_multiallelic <INT>                Max multiallelics per block [default: 2]
         -i, --max_missing <INT>                     Max missing per block [default: 2]
-
+        --popgen_metrics
 """
 
+'''
+--popgen_metrics -B
+causes
+    - global bSFS tally as tsv
+    - samble-set bSFS tally as tsv
+    - popgen-metrics file
+        - overall pi_A, pi_B, dxy, fst
+            intra: [(0.5*m_1 + 0.5*m_2 + m_3+m_4) / blocks for sample_set in intra_sample_sets] / len(intra_sample_sets)
+        - sample-set pi_A, pi_B, dxy, fst
+
+
+'''
 from timeit import default_timer as timer
 from docopt import docopt
 import lib.gimble
