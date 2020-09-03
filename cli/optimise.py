152a4e395f8e564e6c309bba845cd58d4322878b
Gertjan Bisschop <gertjanbisschop@gmail.com>
2020-09-03 17:46:04 +0100

    running gridpoints in parallel

--- cli/optimise.py
+++ cli/optimise.py
@@ -52,25 +52,23 @@ def main(params):
         parameterObj = GridsearchParameterObj(params, args)
         print("[+] Generated all parameter combinations.")
         equationSystem = lib.math.EquationSystemObj(parameterObj)
-        equationSystem.initiate_model()
+        equationSystem.initiate_model(parameterObj)
         
-        gimbleStore = lib.gimble.Store(path=parameterObj.zstore, create=False)
+        #gimbleStore = lib.gimble.Store(path=parameterObj.zstore, create=False)
         #load ETP numpy array
-        if not gimbleStore._is_zarr_group(parameterObj.grid_name, 'grids'):
-            sys.exit("[X] Specified grid not found in zarr store.")
-        grid = zarr.load(gimbleStore.data[f'grids/{parameterObj.grid_name}'])
-        data = gimbleStore.get_bsfs_matrix(
-            data=parameterObj.data_type, 
-            population_by_letter=parameterObj.config['population_ids'], 
-            cartesian_only=True, 
-            kmax_by_mutype=parameterObj.config['k_max'])
+        #if not gimbleStore._is_zarr_group(parameterObj.grid_name, 'grids'):
+        #    sys.exit("[X] Specified grid not found in zarr store.")
+        #grid = zarr.load(gimbleStore.data[f'grids/{parameterObj.grid_name}'])
+        #data = gimbleStore.get_bsfs_matrix(
+        #    data=parameterObj.data_type, 
+        #    population_by_letter=parameterObj.config['population_ids'], 
+        #    cartesian_only=True, 
+        #    kmax_by_mutype=parameterObj.config['k_max'])
         #load data
-        #z=zarr.open('/Users/s1854903/Documents/ongoing_projects/gIMble/output/new_configs/test.z')
-        #data =z['grids/grid_7'][0]
-        #for ETP in equationSystem.ETPs:
-        #    print(lib.math.calculate_composite_likelihood(ETP,data))
+        z=zarr.open('/Users/s1854903/Documents/ongoing_projects/gIMble/output/new_configs/test.z')
+        data =z['grids/grid_7'][0]
         #run optimisation
-        equationSystem.optimise_parameters(data, maxeval=10, localOptimum=False)
+        equationSystem.optimise_parameters(data, maxeval=50, localOptimum=False)
         
         print("[*] Total runtime: %.3fs" % (timer() - start_time))
     except KeyboardInterrupt:
