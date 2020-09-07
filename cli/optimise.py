8a3b370bf87172031a45f361ae9cd67e9ba135c8
Dominik R Laetsch <dominik.laetsch@gmail.com>
2020-09-07 13:05:25 +0100

    cli.optimise
    data

--- cli/optimise.py
+++ cli/optimise.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""usage: gimble optimise                  [-z FILE] -c FILE [-m FILE] [-b|-w] [-t INT] [-h|--help]
+"""usage: gimble optimise                  [-z FILE] -c FILE [-m FILE] [-b|-w|-e] [-t INT] [-h|--help]
                                             
                                             
     Options:
         -h --help                                   show this
         -z, --zarr_file FILE                        ZARR datastore
-        -b, --blocks
-        -w, --windows
-        -c, --config_file FILE
-        -m, --model_file FILE
+        -c, --config_file FILE                      INI config file
+        -m, --model_file FILE                       gimble model TSV
+        -b, --blocks                                Optimise based on blocks 
+        -w, --windows                               Optimise based on windows (might take very long)
+        -e, --etp                                   Optimise based on model (debugging only)
         -t, --threads INT                           Threads [default: 1]
 """
-import pathlib
-import collections
 from timeit import default_timer as timer
 from docopt import docopt
 import sys
@@ -22,13 +21,13 @@ import lib.gimble
 import lib.math
 import zarr
 
-class GridsearchParameterObj(lib.gimble.ParameterObj):
+class OptimiseParameterObj(lib.gimble.ParameterObj):
     '''Sanitises command line arguments and stores parameters.'''
 
     def __init__(self, params, args):
         super().__init__(params)
         self.zstore = self._get_path(args['--zarr_file'])
-        self.data_type = self._get_datatype([args['--blocks'], args['--windows']])
+        self.data_type = self._get_datatype(args)
         self.config_file = self._get_path(args['--config_file'])
         self.model_file = self._get_path(args['--model_file'])
         self.threads = self._get_int(args["--threads"])
@@ -36,25 +35,39 @@ class GridsearchParameterObj(lib.gimble.ParameterObj):
         self._process_config()
 
     def _get_datatype(self, args):
-        if not any(args):
-            return None
-        elif args[0]:
+        choices = [args['--blocks'], args['--windows'], args['--etp']]
+        if all(choices) or not any(choices):
+            sys.exit("[X] Please specify either '--blocks' or '--windows' or '--etp.")
+        if args['--blocks']:
             return 'blocks'
-        elif args[1]:
+        if args['--windows']:
             return 'windows'
-        else:
-            sys.exit("[X1] This should not have happened.")
+        if args['--etp']:
+            return 'etp'
         
 def main(params):
     try:
         start_time = timer()
         args = docopt(__doc__)
-        parameterObj = GridsearchParameterObj(params, args)
+        parameterObj = OptimiseParameterObj(params, args)
         print("[+] Generated all parameter combinations.")
-        equationSystem = lib.math.EquationSystemObj(parameterObj)
-        equationSystem.initiate_model(parameterObj)
-        
-        #gimbleStore = lib.gimble.Store(path=parameterObj.zstore, create=False)
+        if parameterObj.data_type == 'etp':
+            # load math.EquationSystemObj
+            equationSystem = lib.math.EquationSystemObj(parameterObj)
+            # initiate model equations
+            equationSystem.initiate_model(parameterObj)
+            equationSystem.calculate_all_ETPs()
+            equationSystem.optimise_parameters(equationSystem.ETPs, maxeval=50, localOptimum=False)
+        else:
+            gimbleStore = lib.gimble.Store(path=parameterObj.zstore, create=False)
+            if not gimbleStore.has_stage(parameterObj.data_type):
+                sys.exit("[X] GStore has no %r." % parameterObj.data_type)
+            # load math.EquationSystemObj
+            equationSystem = lib.math.EquationSystemObj(parameterObj)
+            # initiate model equations
+            equationSystem.initiate_model(parameterObj)
+            data = gimbleStore.get_bsfs(data_type=parameterObj.data_type, sample_sets="X", kmax_by_mutype=parameterObj.config['k_max'])
+            equationSystem.optimise_parameters(data, maxeval=50, localOptimum=False)
         #load ETP numpy array
         #if not gimbleStore._is_zarr_group(parameterObj.grid_name, 'grids'):
         #    sys.exit("[X] Specified grid not found in zarr store.")
@@ -65,10 +78,10 @@ def main(params):
         #    cartesian_only=True, 
         #    kmax_by_mutype=parameterObj.config['k_max'])
         #load data
-        z=zarr.open('/Users/s1854903/Documents/ongoing_projects/gIMble/output/new_configs/test.z')
-        data =z['grids/grid_7'][0]
+        #z=zarr.open('/Users/s1854903/Documents/ongoing_projects/gIMble/output/new_configs/test.z')
+        #data =z['grids/grid_7'][0]
         #run optimisation
-        equationSystem.optimise_parameters(data, maxeval=50, localOptimum=False)
+        #equationSystem.optimise_parameters(data, maxeval=50, localOptimum=False)
         
         print("[*] Total runtime: %.3fs" % (timer() - start_time))
     except KeyboardInterrupt:
