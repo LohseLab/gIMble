b0c843873b126dfa7adffeffbec0d45d10365020
Dominik R Laetsch <dominik.laetsch@gmail.com>
2021-03-12 13:49:50 +0000

    query
    - format_time
    - explicit arguments
    - log_action

--- cli/query.py
+++ cli/query.py
@@ -1,37 +1,23 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""usage: gimble query                  -z FILE [-b -w -s] [--bed|--simplebed] [-c FILE --lncls] [--bsfs --kmax STR] 
+"""usage: gimble query                  -z DIR [-b|-w|-s] [--bed|--tally|--lncls] [-m STR] [-k STR] 
                                             [-h|--help]
                                             
-    Options:
-        -h --help                                   show this
-        -z, --zarr_file FILE                        ZARR datastore
-        -b, --blocks                                Query data for blocks
-        -w, --windows                               Query data for windows
-        -s, --windows_sum                           Query data for sum of windows
-        -c, --config_file FILE                      INI Config file
-        --bed                                       Writes BED with variation/multiallelic/missing (for -b, -w)
-        --simplebed                                 Writes only BED coordinates (for -w)
-        --bsfs                                      Writes 2D bSFS for data (for -b, -s)
-        --lncls                                     Write grid and lnCls based on an INI file (requires)
-        --kmax STR                                  k-max for mutypes, e.g. [2, 2, 2, 2]
+        -z, --zarr_file DIR                 ZARR datastore
+        -b, --blocks                        Query data for blocks
+        -w, --windows                       Query data for windows
+        -s, --windows_sum                   Query data for sum of windows
+        --bed                               Writes BED with variation/multiallelic/missing (windows)
+        --tally                             Writes 2D bSFS for data (blocks/windows/windows_sum)
+        --lncls                             Write grid and lnCls (for -k <STR>)
+        -m, --maxk STR                      Max value for mutypes (values above get binned), e.g. [2, 2, 2, 2]
+        -k, --key STR                       Query data based on key
 
+        -h --help                           show this
 """
-
-'''
-query based on grid idx/params
-
-
-output CSV
-    for each windows best
-'''
 
 from timeit import default_timer as timer
 from docopt import docopt
 import lib.gimble
 import sys
-import itertools 
 import ast
 
 class QueryParameterObj(lib.gimble.ParameterObj):
@@ -39,25 +25,25 @@ class QueryParameterObj(lib.gimble.ParameterObj):
 
     def __init__(self, params, args):
         super().__init__(params)
+        self.zstore = self._get_path(args['--zarr_file'])
         self.data_type = self._get_data_type(args)
         self.data_format = self._get_data_format(args)
-        self._check_input()
-        self.zstore = self._get_path(args['--zarr_file'])
-        self.kmax_by_mutype = self._get_kmax(args['--kmax'])
-        self.config_file = self._get_path(args['--config_file'])
-        self.config = None
-        self._parse_config(self.config_file)
+        self.kmax_by_mutype = self._get_kmax(args['--maxk'])
+        self.key = args['--key']
+        self._check_input(args)
 
-    def _check_input(self):
+    def _check_input(self, args):
         if self.data_type == 'blocks':
-            if self.data_format == 'bsfs' or self.data_format == 'bed':
+            if self.data_format == 'tally':
                 return True
         if self.data_type == 'windows':
-            if self.data_format == 'bed' or self.data_format == 'lncls' or self.data_format == 'simplebed':
+            if self.data_format == 'bed' or self.data_format == 'lncls' or self.data_format == 'tally':
                 return True
         if self.data_type == 'windows_sum':
-            if self.data_format == 'bsfs' or self.data_format == 'lncls':
+            if self.data_format == 'tally' or self.data_format == 'lncls':
                 return True
+        if self.data_type == None:
+            sys.exit(__doc__)
         sys.exit("[X] Data type %r and data format %r are not supported" % (self.data_type, self.data_format))
 
     def _get_data_type(self, args):
@@ -68,24 +54,22 @@ class QueryParameterObj(lib.gimble.ParameterObj):
         elif args['--windows_sum']:
             return 'windows_sum'
         else:
-            raise ValueError('unknown data_type')
+            return None
 
     def _get_data_format(self, args):
         if args['--bed']:
             return 'bed'
-        elif args['--simplebed']:
-            return 'simplebed'
-        elif args['--bsfs']:
-            return 'bsfs'
+        elif args['--tally']:
+            return 'tally'
         elif args['--lncls']:
             return 'lncls'
         else:
-            raise ValueError('unknown data_format')
+            return None
 
     def _get_kmax(self, kmax_string):
         mutypes = ['m_1', 'm_2', 'm_3', 'm_4']
         if kmax_string == None:
-            return []
+            return None
         try:
             return {mutype: kmax for mutype, kmax in zip(mutypes, ast.literal_eval(kmax_string))}
         except ValueError:
@@ -97,8 +81,13 @@ def main(params):
         args = docopt(__doc__)
         parameterObj = QueryParameterObj(params, args)
         gimbleStore = lib.gimble.Store(path=parameterObj.zstore)
-        gimbleStore.query(parameterObj)
-        print("[*] Total runtime: %.3fs" % (timer() - start_time))
+        gimbleStore.query(
+            parameterObj._VERSION,
+            parameterObj.data_type,
+            parameterObj.data_format,
+            parameterObj.kmax_by_mutype
+            )
+        print("[*] Total runtime was %s" % (lib.gimble.format_time(timer() - start_time)))
     except KeyboardInterrupt:
         print("\n[X] Interrupted by user after %s seconds!\n" % (timer() - start_time))
         exit(-1)
