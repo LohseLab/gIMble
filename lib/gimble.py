1fabd9d654e62d79022321bedf288a094de15691
Dominik R Laetsch <dominik.laetsch@gmail.com>
2022-04-19 15:29:14 +0100

    ...

--- lib/gimble.py
+++ lib/gimble.py
@@ -2507,7 +2507,7 @@ class Store(object):
             config['gridsearch_keys'] = [self._get_key(task='gridsearch', data_label=config['data_label'], analysis_label=grid_meta['label'], parameter_label=idx) for idx in range(meta['max_idx'] + 1)]
         config['block_length_grid'] = grid_meta['block_length']
         print('grid_meta', dict(grid_meta))
-        config['parameters_grid_points'] = grid_meta.get('parameters_grid_points', 
+        config['parameters_grid_points'] = grid_meta.get('parameters_grid_points', None)
         # checking whether block_length in data and grid are compatible
         if not config['block_length_data'] == config['block_length_grid']:
             sys.exit("[X] Block lengths in data %r (%s) and grid %r (%s) are not compatible.." % (
